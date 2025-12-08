46310779c8b0705d69758baefe5c15e2a96de0ae
annu-gif <rrriyarrr@gmail.com>
2025-12-07 19:38:29 -0600

    Initialize Express server in index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// A simple route
+app.get('/', (req, res) => {
+  res.send('Hello from my Dockerized Node.js app on EC2!');
+});
+
+// Start the server
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
